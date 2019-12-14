53a005b86dc397c4d3fd4c859884d69dd54ac580
Andreu Martínez Vidal <andreumv@hotmail.com>
2019-12-14 10:11:10 +0100

    Added packages

--- MyBookWebApp/Program.cs
+++ MyBookWebApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -14,6 +15,7 @@ namespace MyBookWebApp
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            _ = typeof(Microsoft.AspNetCore.Mvc.DataAnnotations.RequiredAttributeAdapter);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
